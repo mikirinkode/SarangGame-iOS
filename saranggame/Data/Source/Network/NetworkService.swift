773a2466df1aa0abe1edfed9a2f0d193ce6f8c8d
mikirinkode <muhammadwafa105@gmail.com>
2024-12-01 17:23:13 +0700

    feat: use alamofire for network fetch

--- saranggame/Data/Source/Network/NetworkService.swift
+++ saranggame/Data/Source/Network/NetworkService.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Alamofire
 
 enum NetworkError: Error {
     case invalidResponse
@@ -17,79 +18,92 @@ class NetworkService {
     let baseURL = "https://api.rawg.io/api"
 
     func getGenreList() async throws -> [GenreEntity] {
-        
-        var components = URLComponents(string: "\(baseURL)/genres")!
-        components.queryItems = [
-            URLQueryItem(name: "key", value: apiKey)
+        let url = "\(baseURL)/genres"
+        let parameters: [String: String] = [
+            "key": apiKey
         ]
-        
-        let request = URLRequest(url: components.url!)
-        let (data, response) = try await URLSession.shared.data(for: request)
-        
-        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
-             throw NetworkError.invalidResponse
-        }
-        
-        do {
-            let decoder = JSONDecoder()
-            let result = try decoder.decode(GenreListResponse.self, from: data)
-            print("result  count: \(result.genreList.count)")
-            return result.toEntities()
-        } catch {
-            throw NetworkError.requestFailed("We encountered an unexpected issue. Please try again.")
+
+        return try await withCheckedThrowingContinuation { continuation in
+            AF.request(url, method: .get, parameters: parameters)
+                .validate(statusCode: 200..<300)
+                .responseDecodable(of: GenreListResponse.self) { response in
+                    switch response.result {
+                    case .success(let result):
+                        print("Result count: \(result.genreList.count)")
+                        continuation.resume(returning: result.toEntities())
+                    case .failure(let error):
+                        if let afError = error.asAFError, afError.isResponseValidationError {
+                            continuation.resume(throwing: NetworkError.invalidResponse)
+                        } else {
+                            continuation.resume(
+                                throwing: NetworkError.requestFailed(
+                                    "We encountered an unexpected issue. Please try again."
+                                )
+                            )
+                        }
+                    }
+                }
         }
     }
-    
+
     func getGameList(_ genreID: String?) async throws -> [GameEntity] {
-        var components = URLComponents(string: "\(baseURL)/games")!
-        var queryItems = [
-            URLQueryItem(name: "key", value: apiKey)
+        let url = "\(baseURL)/games"
+        var parameters: [String: String] = [
+            "key": apiKey
         ]
-        
-        // Only add the "genres" query item if it's not nil
+
         if let genreID = genreID, !genreID.isEmpty {
-            queryItems.append(URLQueryItem(name: "genres", value: genreID))
+            parameters["genres"] = genreID
         }
-        
-        components.queryItems = queryItems
-        let request = URLRequest(url: components.url!)
-        
-        let (data, response) = try await URLSession.shared.data(for: request)
-        
-        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
-            throw NetworkError.invalidResponse
-        }
-        
-        do {
-            let decoder = JSONDecoder()
-            let result = try decoder.decode(GameListResponse.self, from: data)
-            return result.toEntities()
-        } catch {
-            throw NetworkError.requestFailed("We encountered an unexpected issue. Please try again.")
+
+        return try await withCheckedThrowingContinuation { continuation in
+            AF.request(url, method: .get, parameters: parameters)
+                .validate(statusCode: 200..<300)
+                .responseDecodable(of: GameListResponse.self) { response in
+                    switch response.result {
+                    case .success(let result):
+                        continuation.resume(returning: result.toEntities())
+                    case .failure(let error):
+                        if let afError = error.asAFError, afError.isResponseValidationError {
+                            continuation.resume(throwing: NetworkError.invalidResponse)
+                        } else {
+                            continuation.resume(
+                                throwing: NetworkError.requestFailed(
+                                    "We encountered an unexpected issue. Please try again."
+                                )
+                            )
+                        }
+                    }
+                }
         }
     }
-    
+
     func getGameDetail(_ gameID: String) async throws -> GameDetailEntity {
-        var components = URLComponents(string: "\(baseURL)/games/")!
-        
-        components.path.append("\(gameID)")
-        
-        components.queryItems = [URLQueryItem(name: "key", value: apiKey)]
-        
-        let request = URLRequest(url: components.url!)
-        
-        let (data, response) = try await URLSession.shared.data(for: request)
-        
-        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
-            throw NetworkError.invalidResponse
-        }
-        
-        do {
-            let decoder = JSONDecoder()
-            let result = try decoder.decode(GameDetailResponse.self, from: data)
-            return result.toEntity()
-        } catch {
-            throw NetworkError.requestFailed("We encountered an unexpected issue. Please try again.")
+        let url = "\(baseURL)/games/\(gameID)"
+        let parameters: [String: String] = [
+            "key": apiKey
+        ]
+
+        return try await withCheckedThrowingContinuation { continuation in
+            AF.request(url, method: .get, parameters: parameters)
+                .validate(statusCode: 200..<300)
+                .responseDecodable(of: GameDetailResponse.self) { response in
+                    switch response.result {
+                    case .success(let result):
+                        continuation.resume(returning: result.toEntity())
+                    case .failure(let error):
+                        if let afError = error.asAFError, afError.isResponseValidationError {
+                            continuation.resume(throwing: NetworkError.invalidResponse)
+                        } else {
+                            continuation.resume(
+                                throwing: NetworkError.requestFailed(
+                                    "We encountered an unexpected issue. Please try again."
+                                )
+                            )
+                        }
+                    }
+                }
         }
     }
+
 }
